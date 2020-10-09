ee7e0a31bed2445a07a849a41a1532c4d5db8f34
Joshua Hoffman <joshuahoffman2023@u.northwestern.edu>
2020-10-09 15:37:58 -0500

    Initial Commit

---
+++ Trick or Treat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trick or Treat
+//
+//  Created by Joshua Hoffman on 10/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trick or Treat/Trick_or_TreatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trick_or_TreatApp.swift
+//  Trick or Treat
+//
+//  Created by Joshua Hoffman on 10/9/20.
+//
+
+import SwiftUI
+
+@main
+struct Trick_or_TreatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
